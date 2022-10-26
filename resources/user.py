9be4ba48ca21e21efa49b2a367ffd5e3f22b95ae
gmassalas <gmassalas@pi-ag.com>
2022-10-26 21:15:49 +0300

    fix in users resource

--- resources/user.py
+++ resources/user.py
@@ -87,7 +87,7 @@ class UserLogout(Resource):
         jti = get_jwt()["jti"]  # jti is "JWT ID", a unique identifier for a JWT.
         user_id = get_jwt_identity()
         BLOCKLIST.add(jti)
-        return {"message": USER_LOGGED_OUT.format(user_id)}, 200
+        return {"message": USER_LOGGED_OUT.format(user_id=user_id)}, 200
 
 
 class TokenRefresh(Resource):
